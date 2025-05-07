9828583d446bf4bb0f6a1c2dd813656846c24e80
meghana242005 <meghana242005@gmail.com>
2025-05-07 15:23:51 +0530

    save

---
+++ basicopration.java
@@ -0,0 +1,16 @@
+public class basicopration
+ {
+    
+    public static void main(String[] args) {
+      int a = 15;
+      int b= 5;
+      int sum  = a+b ;
+      int diff = a-b;
+      int mul = a*b;
+       System.out.println("Addition: " + sum);
+       System.out.println("Subtration : " + diff);
+       System.out.println("Multiplication : " + mul);
+       
+    }
+      
+  }
